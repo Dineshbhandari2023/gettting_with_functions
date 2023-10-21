51d67076ae9bda623d30446c358692f9374ef956
Dinesh Bhandari <133768059+Dineshbhandari2023@users.noreply.github.com>
2023-10-22 00:29:20 +0545

    Update cisar_ciper.py

--- cisar_ciper.py
+++ cisar_ciper.py
@@ -1,19 +1,26 @@
 
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, \n And, Type 'decode' to decript. \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number that you want: \n"))
-
+shift = shift % 26
 
 def caesar(start_text, shift_amount, ciper_direction):
     end_text = ""
     if ciper_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {ciper_direction}d text is {end_text}.")
-
-caesar(start_text=text, shift_amount=shift, ciper_direction=direction)
+should_continue = True
+while should_continue:
+    caesar(start_text=text, shift_amount=shift, ciper_direction=direction)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    if result == "no":
+        should_continue = False
+    print("GoodBye!!")
